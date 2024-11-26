cfb316f2418ec194721330865d2f6c90f4d4677d
KushKK29 <kush282930@gmail.com>
2024-11-26 23:56:14 +0530

    ttgjt

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,10 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-<<<<<<< HEAD
   <InfoProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </InfoProvider>
   
-=======
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
->>>>>>> dc505a563077ce7be4e9e594c4e74267e09500d2
 );
